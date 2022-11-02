946fcb9394b6c84e306552f0704331bc380e54f0
sakait-pc <mhdksamd@gmail.com>
2022-11-02 15:09:46 +0900

    fix: meta info

--- components/Layout.js
+++ components/Layout.js
@@ -10,16 +10,6 @@ export default function Layout({children, title}) {
       <div className={styles.container}>
         <Head>
           <link rel="icon" href="/favicon.ico" />
-          <meta
-            name="description"
-            content="A personal website created by sakait-pc"
-          />
-          <meta
-            property="og:image"
-            content={`https://og-image.vercel.app/${encodeURI(siteTitle)}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-          />
-          <meta name="og:title" content={siteTitle} />
-          <meta name="twitter:card" content="summary_large_image" />
           <title>{title}</title>
           <meta name="google-site-verification" content="g0Mvuu_M1JqGTuvUqHRnF-dS6eRa9QA6jbJCSsI6XAM" />
         </Head>
