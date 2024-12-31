e68211bece0471fe4ac97d1e43aef20ccc206894
sakait-pc <mhdksamd@gmail.com>
2024-12-31 14:15:49 +0900

    fix: remove Host rule
    - Search Consoleの[設定]でrobots.txtにHostルールを無視したという警告が出ていた
    ため、ビルド時に取り除くよう修正した。
    - next-sitemapのtransformRobotsTxtを使うためv3.0.3に更新した。

--- sitemap.config.js
+++ sitemap.config.js
@@ -1,6 +1,15 @@
 module.exports = {
   siteUrl: 'https://sakait-blog.web.app',
   generateRobotsTxt: true,
+  robotsTxtOptions: {
+    transformRobotsTxt: async (_, robotsTxt) => {
+      return robotsTxt
+        .split('\n')
+        .filter(line => !line.startsWith('# Host') && !line.startsWith('Host:'))
+        .join('\n')
+        .replace(/\n{2,}/g, '\n\n');
+      },
+  },
   sitemapSize: 7000,
   outDir: './out',
 };
